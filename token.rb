055815d4ced69f091e05c3605ad4e2f0cd3c08b4
ls1955 <xxlsxx1955@gmail.com>
2023-06-30 21:12:44 +0800

    Include token.rb
    Insert some keywords inside the file.

---
+++ token.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# As filename suggest, this file contain token and some keywords
+# use inside the Monkey programming language.
+
+# Go code from books
+# As Author is unfamilliar with Go,
+# will leave them at there for future reference
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# type TokenType string
+
+# type Token struct {
+#   Type TokenType
+#   Literal string
+# }
+# 
+# const (
+#   ILLEGAL = "ILLEGAL"
+#   ...
+# )
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+module Monkey
+  module Token
+    # An unknown token/character
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    # Identifiers + literals
+    IDENTIFIER = "IDENTIFIER"
+    INT = "INT"
+
+    # Operators
+    ASSIGN = "="
+    PLUS = "+"
+
+    # Delimiters
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    # Keywords
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+  end
+end
