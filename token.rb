5997e3926b89f24f1647af45601a44309dd4f653
ls1955 <xxlsxx1955@gmail.com>
2023-07-04 21:41:11 +0800

    Rename Token module to TokenType
    After rereaded the book, it seems like author had mistaken
    token types as token themselves.
    The comments at the upper part of the file had been removed
    as author deemed them unncessary.

--- token.rb
+++ token.rb
@@ -1,29 +1,9 @@
 # frozen_string_literal: true
 
-# As filename suggest, this file contain token and some keywords
-# use inside the Monkey programming language.
-
-# Go code from books
-# As Author is unfamilliar with Go,
-# will leave them at there for future reference
-# # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# type TokenType string
-
-# type Token struct {
-#   Type TokenType
-#   Literal string
-# }
-# 
-# const (
-#   ILLEGAL = "ILLEGAL"
-#   ...
-# )
-# # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 module Monkey
   Token = Struct.new :type, :literal, keyword_init: true
 
-  module Token
+  module TokenType
     # An unknown token/character
     ILLEGAL = "ILLEGAL"
     EOF = "EOF"
