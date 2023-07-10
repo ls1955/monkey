9b12c555a610a9736fa6fe432f5ed045e99973b1
ls1955 <xxlsxx1955@gmail.com>
2023-07-10 20:21:10 +0800

    Include second lexer test
    This test:
    * Include identifiers that could consists of several character
    * Include two valid variable assignment
    * Include a valid function declaration statement

--- test/lexer_test.rb
+++ test/lexer_test.rb
@@ -9,7 +9,7 @@ module Monkey
   class MonkeyLexerTest < Minitest::Test
     def test_lexer_tokenise_input_one
       input = "=+(){},;"
-      lexer = Lexer.new input: input
+      lexer = Lexer.new(input:)
 
       exps = [
         [TokenType::ASSIGN, "="],
@@ -28,6 +28,69 @@ module Monkey
 
         assert_equal exps[i][0], token.type
         assert_equal exps[i][1], token.literal
+      end
+    end
+
+    def test_lexer_tokenise_input_two
+      input = <<~INPUT
+        let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+          x + y;
+        };
+
+        let result = add(five, ten);
+      INPUT
+      lexer = Lexer.new(input:)
+
+      exps = [
+        [TokenType::LET, "let"],
+        [TokenType::IDENT, "five"],
+        [TokenType::ASSIGN, "="],
+        [TokenType::INT, 5],
+        [TokenType::SEMICOLON, ";"],
+        [TokenType::LET, "let"],
+        [TokenType::IDENT, "ten"],
+        [TokenType::ASSIGN, "="],
+        [TokenType::INT, 10],
+        [TokenType::SEMICOLON, ";"],
+        [TokenType::LET, "let"],
+        [TokenType::IDENT, "add"],
+        [TokenType::ASSIGN, "="],
+        [TokenType::FUNCTION, "fn"],
+        [TokenType::LPAREN, "("],
+        [TokenType::IDENT, "x"],
+        [TokenType::COMMA, ","],
+        [TokenType::IDENT, "y"],
+        [TokenType::RPAREN, ")"],
+        [TokenType::LBRACE, "{"],
+        [TokenType::IDENT, "x"],
+        [TokenType::PLUS, "+"],
+        [TokenType::IDENT, "y"],
+        [TokenType::SEMICOLON, ";"],
+        [TokenType::RBRACE, "}"],
+        [TokenType::SEMICOLON, ";"],
+        [TokenType::LET, "let"],
+        [TokenType::IDENT, "result"],
+        [TokenType::ASSIGN, "="],
+        [TokenType::IDENT, "add"],
+        [TokenType::LPAREN, "("],
+        [TokenType::IDENT, "five"],
+        [TokenType::COMMA, ","],
+        [TokenType::IDENT, "ten"],
+        [TokenType::RPAREN, ")"],
+        [TokenType::SEMICOLON, ";"]
+      ]
+
+      index = 0
+      until lexer.char.type == TokenType::EOF
+        token = lexer.next_token
+
+        assert_equal exps[index][0], token.type
+        assert_equal exps[index][1], token.literal
+
+        index += 1
       end
     end
   end
