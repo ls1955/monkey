136f2d48d4928538589ff9d2be96b90cfeba0f69
ls1955 <xxlsxx1955@gmail.com>
2023-07-12 19:08:17 +0800

    Include #next_identifier
    Because:
    * Lexer should have the ability to parse keywords or identifiers
      that consists of multiple characters
    * Lexer should have the ability to identify legal identifier_letter
    
    This commit:
    * Include #next_identifier where lexer keep reading valid letter
    * Include #valid_identifier_letter?
    * Fix a bug at #read_and_advance (@position -> @curr_pos), as
      @position should not exist.
    * Allow lexer to return IDENTIFIER token in #next_token

--- lexer.rb
+++ lexer.rb
@@ -39,6 +39,9 @@ module Monkey
         Token.new type: TokenType::SEMICOLON, literal: curr_char
       when 0
         Token.new type: TokenType::EOF, literal: ""
+      else
+        identifier = next_identifier
+        Token.new type: TokenType::IDENTIFIER, literal: identifier
       end
     end
     # rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity
@@ -46,8 +49,24 @@ module Monkey
     # Read a char at next_read_pos, and set curr_char with that character
     def read_and_advance
       @curr_char = next_read_pos < input.length ? input[next_read_pos] : 0
-      @position = next_read_pos
+      @curr_pos = next_read_pos
       @next_read_pos += 1
+    end
+
+    # Keep reading the valid characters and return it as a substring
+    def next_identifier
+      start_pos = curr_pos
+      read_and_advance while valid_identifier_letter?(curr_char)
+      input[start_pos...curr_pos]
+    end
+
+    private
+
+    def valid_identifier_letter?(char)
+      # Check if char is 0 (Integer) which occured at EOF
+      return false unless char.is_a? String
+
+      char.match? /[a-zA-Z0-9_]/
     end
   end
 end
