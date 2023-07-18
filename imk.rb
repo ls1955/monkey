32fc2bc2ad23b4080ce739a3c7eaac66185a162f
ls1955 <xxlsxx1955@gmail.com>
2023-07-18 21:30:25 +0800

    Include imk.rb
    Because:
    REPL is a cool thing to have, no?
    
    This commit:
    * Implement IMK#execute that run the loop until the end
      of time
    * Implement IMK#print_intro

---
+++ imk.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: false
+
+require_relative "lexer"
+
+# :nodoc:
+module Monkey
+  # IMK stands for "Interactive MonKey", it is the REPL for Monkey
+  class IMK
+    # The main loop which for now print every tokens for user inputs
+    def execute
+      print_intro
+
+      print ">> "
+      input = gets.strip
+
+      until input == ""
+        lexer = Lexer.new(input:)
+
+        puts ""
+        lexer.each_token { puts _1 }
+        puts ""
+
+        print ">> "
+        input = gets.strip
+      end
+    end
+
+    def print_intro
+      puts <<~INTRO
+        ------------------------------------------------------------
+          Type any expression to see the tokens that lexer produce.
+          Press Ctrl + D to exit.
+        ------------------------------------------------------------
+      INTRO
+    end
+  end
+end
+
+Monkey::IMK.new.execute
