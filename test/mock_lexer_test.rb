5ec86efb519c04f90fc23f1ec95fccbd069afb4a
Cheong <xxlsxx1955@gmail.com>
2023-08-21 10:17:04 +0800

    Include TokenType module in mock_lexer_test
    Because:
    Save some space by not typing namespace.

--- test/mock_lexer_test.rb
+++ test/mock_lexer_test.rb
@@ -6,11 +6,13 @@ require_relative "../token"
 
 module Monkey
   class MockLexerTest < Minitest::Test
+    include TokenType
+
     def test_it_return_correct_next_token
-      token_one = Token.new type: TokenType::IDENTIFIER, literal: "kuma"
-      token_two = Token.new type: TokenType::INT, literal: 42
-      token_three = Token.new type: TokenType::FUNCTION, literal: "fn"
-      token_four = Token.new type: TokenType::IDENTIFIER, literal: "print_crap"
+      token_one = Token.new type: IDENTIFIER, literal: "kuma"
+      token_two = Token.new type: INT, literal: 42
+      token_three = Token.new type: FUNCTION, literal: "fn"
+      token_four = Token.new type: IDENTIFIER, literal: "print_crap"
       tokens = [token_one, token_two, token_three, token_four]
 
       mock = LexerMock.new(tokens:)
