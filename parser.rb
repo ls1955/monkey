d24ec4c2ce32f2f8057e15bd8da3d053870200bc
Cheong <xxlsxx1955@gmail.com>
2023-08-21 17:41:32 +0800

    Include parser

---
+++ parser.rb
@@ -0,0 +1,19 @@
+module Monkey
+  # The almighty lexer for Monkey programming language
+  class Parser
+    attr_reader :curr_token, :lexer, :peek_token
+
+    def initialize(lexer:)
+      @lexer = lexer
+
+      # BECAUSE:
+      # To setup both curr_token and peek_token before anything else
+      2.times { next_token }
+    end
+
+    def next_token
+      @curr_token = peek_token
+      @peek_token = lexer.next_token
+    end
+  end
+end
