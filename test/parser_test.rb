eb8a81c9951a0fe942526d9ea68638f582492659
ls1955 <xxlsxx1955@gmail.com>
2023-09-11 21:13:35 +0800

    Include failing parser test

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -25,6 +25,28 @@ module Monkey
       assert_equal tokens[0], parser.curr_token
       assert_equal tokens[1], parser.peek_token
     end
+
+    def test_parse_program_from_lexer
+      input = <<~INPUT
+        let num = 1;
+        let char = "a";
+        let secret_of_universe = 43;
+      INPUT
+
+      lexer = Lexer.new(input:)
+      parser = Parser.new(lexer:)
+
+      program = parser.parse
+
+      assert program
+      assert_equal 3, program.size
+
+      %w(num char secret_of_universe).zip(program.statements).each do |exp_ident, let_statement|
+        assert_equal "let", let_statement.token_literal
+        assert_equal exp_ident, let_statement.name.val
+        assert_equal exp_ident, let_statement.name.token_literal
+      end
+    end
   end
 end
 # rubocop:enable Metrics/MethodLength
